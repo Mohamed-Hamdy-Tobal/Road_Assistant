7cd6c147daca4fc4d5636e5e589e45742b1a1a12
DESKTOP-F42UCN8\7azem_store <mohamed.tolba@lastcloud.com.au>
2024-05-23 16:12:17 +0300

    Clean Test

--- app/[locale]/test/page.jsx
+++ app/[locale]/test/page.jsx
@@ -1,22 +1,9 @@
-import React from 'react';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import React from 'react'
 
-const IndexPage = () => {
-    const position = [51.505, -0.09]
-
-    render(
-        <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={position}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
-        </MapContainer>
+const Test = () => {
+    return (
+        <div>Test</div>
     )
-};
+}
 
-export default IndexPage;
+export default Test

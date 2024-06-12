4621bae0ce8208f17959c078fe79b2bfcb09a51c
DESKTOP-F42UCN8\7azem_store <mohamed.tolba@lastcloud.com.au>
2024-06-12 17:02:48 +0300

    update in dash

--- components/Dashboard/DashboardContent.jsx
+++ components/Dashboard/DashboardContent.jsx
@@ -290,7 +290,7 @@ const DashboardContent = () => {
 
 
                 <div className='relative overflow-x-auto'>
-                    <table className='min-h-[350px] w-full text-sm text-left rtl:text-right text-gray-500 '>
+                    <table className='w-full text-sm text-left rtl:text-right text-gray-500 '>
                         <thead className='text-xs text-gray-700 uppercase border-b bg-gray-50  '>
                             <tr>
                                 <th scope="col" className="px-6 py-3">Name</th>

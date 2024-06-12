940a4b907a1524edc50105cd05ca505b17dabaef
DESKTOP-F42UCN8\7azem_store <mohamed.tolba@lastcloud.com.au>
2024-06-12 20:20:48 +0300

    Update To Replace Router For Not-Found

--- components/LinkHome.jsx
+++ components/LinkHome.jsx
@@ -1,17 +1,27 @@
+"use client";
+
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons';
+import { useRouter } from 'next/navigation'
 import Link from "next/link";
 
 const LinkHome = () => {
+    // Initialize useRouter hook
+    const router = useRouter();
 
     return (
         <div>
-            <Link href={`/en`} className="absolute top-[30px] left-[30px] label-4o4 transition-all text-secoColor hover:text-orange-300 font-medium text-[25px]">
+            {/* <Link href={`/en`} className="absolute top-[30px] left-[30px] label-4o4 transition-all text-secoColor hover:text-orange-300 font-medium text-[25px]">
                 <div className='home-route'>
                     <span><FontAwesomeIcon icon={faLongArrowAltLeft} /></span><span>Home</span>
                 </div>
-            </Link>
+            </Link> */}
+            <button className="absolute top-[30px] left-[30px] label-4o4 transition-all text-secoColor hover:text-orange-300 font-medium text-[25px]" type="button" onClick={() => router.replace('/en')}>
+                <div className='home-route'>
+                    <span><FontAwesomeIcon icon={faLongArrowAltLeft} /></span><span>Home</span>
+                </div>
+            </button>
         </div>
     )
 }
